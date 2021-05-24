fa2d01e76fe77ca4701445d51e5e582bf6804389
Jessica Tiberio <32365549+JTibs18@users.noreply.github.com>
2021-05-24 13:50:37 -0400

    problem 1

---
+++ PracticeQ1.py
@@ -0,0 +1,23 @@
+# Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+def addUp (nums, target):
+    checkedVal = {}
+    for index, val in enumerate(nums):
+        remain = target - val
+        if remain in checkedVal.keys():
+            return [checkedVal[remain], index]
+        checkedVal[val] = index
+
+arr1 = [2, 7, 11, 15]
+target = 9
+print("indexes: ", addUp(arr1, target))
+
+arr2 = [3, 2, 4]
+target = 6
+print("indexes: ", addUp(arr2, target))
+
+arr3 = [3,3]
+target = 6
+print("indexes: ", addUp(arr3, target))
