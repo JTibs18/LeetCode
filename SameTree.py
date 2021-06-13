28ba2ba67948a5847f3297d156826a6f87ed92ba
Jessica Tiberio <32365549+JTibs18@users.noreply.github.com>
2021-06-13 01:08:39 -0400

    ✅ Binary tree comparison problem

--- SameTree.py
+++ SameTree.py
@@ -1,5 +1,5 @@
-# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
-# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+# Given the roots of two binary trees p and q, write a function to check if they are the same or not
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value
 
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
