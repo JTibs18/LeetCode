eaeb2bae1bafe80192231b8646e8dd2244c72329
Jessica Tiberio <jessicatiberio18@gmail.com>
2021-06-24 00:12:42 -0400

    ✅ stacks and strings

---
+++ ValidParentheses.py
@@ -0,0 +1,41 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if:
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+
+def isValid(s):
+    stack = []
+    for index, value in enumerate(s):
+        if len(stack) == 0:
+            stack.append(value)
+        elif value == ")":
+            if stack.pop() != "(":
+                return False
+        elif value == "]":
+             if stack.pop() != "[":
+                 return False
+        elif value == "}":
+            if stack.pop() != "{":
+                return False
+        else:
+            stack.append(value)
+    if len(stack) > 0:
+        return False
+    else:
+        return True
+
+#Test cases
+s = "()"
+print(isValid(s))
+
+s = "()[]{}"
+print(isValid(s))
+
+s = "(]"
+print(isValid(s))
+
+s = "([)]"
+print(isValid(s))
+
+s = "{[]}"
+print(isValid(s))
