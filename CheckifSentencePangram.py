46e83ee2207524c958d52ed3f7879c12a7999bc5
Jessica Tiberio <jessicatiberio18@gmail.com>
2021-07-16 00:51:23 -0400

    ✅ added another possible function with set instead of list

--- CheckifSentencePangram.py
+++ CheckifSentencePangram.py
@@ -1,6 +1,7 @@
 # A pangram is a sentence where every letter of the English alphabet appears at least once.
 # Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
 
+#Using list to store unique characters
 def checkIfPangram(sentence):
     letters = []
 
@@ -16,12 +17,30 @@ def checkIfPangram(sentence):
     else:
         return False
 
+#Using set to store unique characters
+def checkIfPangram2(sentence):
+    letters = set()
+
+    if len(sentence) < 26:
+        return False
+
+    for char in sentence:
+        letters.add(char)
+
+    if len(letters) == 26:
+        return True
+    else:
+        return False
+
 #Test cases
 sentence = "thequickbrownfoxjumpsoverthelazydog"
 print(checkIfPangram(sentence))
+print(checkIfPangram2(sentence))
 
 sentence = "leetcode"
 print(checkIfPangram(sentence))
+print(checkIfPangram2(sentence))
 
 sentence = "uwqohxamknblecdtzpisgvyfjr"
 print(checkIfPangram(sentence))
+print(checkIfPangram2(sentence))
