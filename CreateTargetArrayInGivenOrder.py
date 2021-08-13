3dff0bb5408f39eea00eedeff472138e5786e4c0
Jessica Tiberio <32365549+JTibs18@users.noreply.github.com>
2021-08-13 19:39:19 -0400

    ✅ iter through array, insert val into target arr with index from 2nd arr

---
+++ CreateTargetArrayInGivenOrder.py
@@ -0,0 +1,27 @@
+# Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+# Initially target array is empty.
+# From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+# Repeat the previous step until there are no elements to read in nums and index.
+# Return the target array.
+# It is guaranteed that the insertion operations will be valid.
+
+def createTargetArray(nums, index):
+    target = []
+
+    for indx, val in enumerate(nums):
+        target.insert(index[indx], val)
+
+    return target
+
+#Test cases
+nums = [0, 1, 2, 3, 4]
+index = [0, 1, 2, 2, 1]
+print(createTargetArray(nums, index))
+
+nums = [1, 2, 3, 4, 0]
+index = [0, 1, 2, 3, 0]
+print(createTargetArray(nums, index))
+
+nums = [1]
+index = [0]
+print(createTargetArray(nums, index))
