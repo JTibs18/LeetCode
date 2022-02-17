91598d46ab0f9779169530c6de2200d23b9e030d
Jessica Tiberio <32365549+JTibs18@users.noreply.github.com>
2022-02-16 20:19:46 -0500

    ✅ n-ary tree preorder traversal with recursion and a helper func

---
+++ N-aryTreePreorderTraversal.py
@@ -0,0 +1,59 @@
+# Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+# Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+def preOrder(root):
+
+    def helperFun(node, output):
+        if node == None:
+            return None
+
+        output.append(node.val)
+
+        if node.children != None:
+            for i in node.children:
+                helperFun(i, output)
+
+        return output
+
+    return helperFun(root, [])
+
+#Test Cases:
+root = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
+root.children = [n3, n2, n4]
+n3.children = [n5, n6]
+print(preOrder(root))
+
+root = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
+n7 = Node(7)
+n8 = Node(8)
+n9 = Node(9)
+n10 = Node(10)
+n11 = Node(11)
+n12 = Node(12)
+n13 = Node(13)
+n14 = Node(14)
+root.children = [n2, n3, n4, n5]
+n3.children = [n6, n7]
+n7.children = [n11]
+n11.children = [n14]
+n4.children = [n8]
+n8.children = [n12]
+n5.children = [n9, n10]
+n9.children = [n13]
+print(preOrder(root))
