d726ae898aa7e5030eb7123dac08c1c7de0f9ae4
Jessica Tiberio <jessicatiberio18@gmail.com>
2023-02-17 23:26:03 -0500

    ✅ very easy math

---
+++ AddTwoIntegers.py
@@ -0,0 +1,13 @@
+# Given two integers num1 and num2, return the sum of the two integers.
+
+def sum(num1, num2): 
+    return num1 + num2
+
+# Test cases
+num1 = 12
+num2 = 5
+print(sum(num1, num2))
+
+num1 = -10 
+num2 = 4 
+print(sum(num1, num2))
