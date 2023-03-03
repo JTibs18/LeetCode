85372fe3cfc97e2911516c838e07bb8c1df532d7
Jessica Tiberio <jessicatiberio18@gmail.com>
2023-03-03 01:59:55 -0500

    database

---
+++ FindTotalTimeSpentByEachEmployeeSchema.sql
@@ -0,0 +1,17 @@
+-- Write an SQL query to calculate the total time in minutes spent by each employee on each day at the office. Note that within one day, an employee can enter and leave more than once. The time spent in the office for a single entry is out_time - in_time.
+-- Return the result table in any order.
+--SCHEMA
+
+BEGIN TRANSACTION; 
+
+DROP TABLE IF EXISTS Employees; 
+
+Create table if Not Exists Employees (emp_id int, event_day date, in_time int, out_time int);
+
+insert into Employees (emp_id, event_day, in_time, out_time) values ('1', '2020-11-28', '4', '32');
+insert into Employees (emp_id, event_day, in_time, out_time) values ('1', '2020-11-28', '55', '200');
+insert into Employees (emp_id, event_day, in_time, out_time) values ('1', '2020-12-3', '1', '42');
+insert into Employees (emp_id, event_day, in_time, out_time) values ('2', '2020-11-28', '3', '33');
+insert into Employees (emp_id, event_day, in_time, out_time) values ('2', '2020-12-9', '47', '74');
+
+COMMIT; 
