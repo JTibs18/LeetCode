285926c51d4ed98084ec94769f18170fe3a20b32
Jessica Tiberio <jessicatiberio18@gmail.com>
2023-03-02 22:46:52 -0500

    ✅ two pointers, string, string matching

---
+++ FindIndexFirstOccurenceString.js
@@ -0,0 +1,29 @@
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+function strStr(haystack, needle){
+    var ptr1 = 0 
+    var ptr2 = needle.length
+
+    while (ptr2 <= haystack.length){
+        if (needle == haystack.slice(ptr1, ptr2)){
+            return ptr1 
+        }
+        ptr1 += 1 
+        ptr2 += 1 
+    }
+    return -1 
+
+  };
+
+// Test cases 
+haystack = "sadbutsad"
+needle = "sad"
+console.log(strStr(haystack, needle))
+
+haystack = "leetcode"
+needle = "leeto"
+console.log(strStr(haystack, needle))
+
+haystack = "a"
+needle = "a"
+console.log(strStr(haystack, needle))
