275fcec022ebd7702a0014695c875c4a3b01b696
Jessica Tiberio <jessicatiberio18@gmail.com>
2023-03-04 03:17:27 -0500

    array, hash table

---
+++ GroupPeopleGivenGroupSizeTheyBelongTo.js
@@ -0,0 +1,33 @@
+// There are n people that are split into some unknown number of groups. Each person is labeled with a unique ID from 0 to n - 1.
+// You are given an integer array groupSizes, where groupSizes[i] is the size of the group that person i is in. For example, if groupSizes[1] = 3, then person 1 must be in a group of size 3.
+// Return a list of groups such that each person i is in a group of size groupSizes[i].
+// Each person should appear in exactly one group, and every person must be in a group. If there are multiple answers, return any of them. It is guaranteed that there will be at least one valid solution for the given input.
+
+function groupThePeople(groupSizes) {
+    var groups = {};
+    var answer = [];
+
+    for (i = 0; i < groupSizes.length; i++){
+        if (groupSizes[i] in groups){
+            groups[groupSizes[i]].push(i);
+        }else{
+            groups[groupSizes[i]] = [i];
+        }
+    }
+
+    for (var [key, value] of Object.entries(groups)){
+        while (value.length != 0){
+            answer.push(value.slice(0, key));
+            value = value.slice(key);
+        }
+    }
+
+    return answer;
+}
+
+// Test cases
+groupSizes = [3,3,3,3,3,1,3]
+console.log(groupThePeople(groupSizes))
+
+groupSizes = [2,1,3,3,3,2]
+console.log(groupThePeople(groupSizes))
