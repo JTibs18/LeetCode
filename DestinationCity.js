adbf706670881e23549b01fd46422103b2270417
Jessica Tiberio <jessicatiberio18@gmail.com>
2023-12-14 19:41:18 -0500

    ✅ string, array

---
+++ DestinationCity.js
@@ -0,0 +1,24 @@
+// You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+// It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+function destCity(paths){
+    source = []
+    destination =[]
+
+    for (i = 0; i < paths.length; i++){
+        source.push(paths[i][0])
+        destination.push(paths[i][1])
+    }
+
+    return destination.filter(x => !source.includes(x))[0]
+} 
+
+// Test cases
+paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+console.log(destCity(paths))
+
+paths = [["B","C"],["D","B"],["C","A"]]
+console.log(destCity(paths))
+
+paths = [["A","Z"]]
+console.log(destCity(paths))
