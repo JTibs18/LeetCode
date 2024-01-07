2d4e1a53a2aa4dee2c034094ce20cafea358eae4
Jessica Tiberio <jessicatiberio18@gmail.com>
2024-01-07 18:31:34 -0500

    ✅ tree, depth first search, binary tree

--- SumOfRootToLeafBinaryNumbers.py
+++ SumOfRootToLeafBinaryNumbers.py
@@ -4,10 +4,29 @@
 # For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
 # Return the sum of these numbers. The answer is guaranteed to fit in a 32-bits integer.
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 def sumRootToLeaf(root):
-    
+    total = 0 
 
+    def helperFun(curNode, total, curBinary):
+        newBinary = curBinary + str(curNode.val)
 
+        if not curNode.left and not curNode.right:
+            return int(newBinary, 2) + total 
+        
+        if curNode.left and curNode.right:
+            return helperFun(curNode.left, total, newBinary) + helperFun(curNode.right, total, newBinary)
+        elif curNode.left:
+            return helperFun(curNode.left, total, newBinary)
+        elif curNode.right:
+            return helperFun(curNode.right, total, newBinary)
+
+    return helperFun(root, total, "")    
 
 #Test Cases
 r = TreeNode(1)
@@ -23,4 +42,12 @@ node1.left = node3
 node1.right = node4
 node2.left = node5
 node2.right = node6
-print(sumOfLeftLeaves(r))
+print(sumRootToLeaf(r))
+
+r = TreeNode(0)
+print(sumRootToLeaf(r))
+
+r = TreeNode(1)
+node1 = TreeNode(1)
+r.left = node1
+print(sumRootToLeaf(r))
