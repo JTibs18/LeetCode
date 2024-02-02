5ac927dd5f80e9de1645f95a8be29d666bec1a9e
Jessica Tiberio <jessicatiberio18@gmail.com>
2024-02-02 03:59:59 -0500

    ✅ array, string

---
+++ NumberOfSeniorCitizens.py
@@ -0,0 +1,22 @@
+# You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+#   The first ten characters consist of the phone number of passengers.
+#   The next character denotes the gender of the person.
+#   The following two characters are used to indicate the age of the person.
+#   The last two characters determine the seat allotted to that person.
+# Return the number of passengers who are strictly more than 60 years old.
+
+def countSeniors(details):
+    seniorCount = 0
+    
+    for i in details:
+        if int(i[11:13]) > 60:
+            seniorCount += 1
+
+    return seniorCount
+
+# Test cases
+details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+print(countSeniors(details))
+
+details = ["1313579440F2036","2921522980M5644"] 
+print(countSeniors(details))
