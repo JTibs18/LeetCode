4e7676f99fb8d77b7b662fb069f16d380dfb70be
Jessica Tiberio <jessicatiberio18@gmail.com>
2024-03-04 14:33:36 -0500

    ✅ typescript json

---
+++ IsObjectEmpty.ts
@@ -0,0 +1,16 @@
+// Given an object or an array, return if it is empty.
+//  An empty object contains no key-value pairs.
+//  An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
+
+function isEmpty(obj: Obj): boolean {
+    return Object.keys(obj).length ? false : true;
+};
+
+// Test cases
+console.log(isEmpty({"x": 5, "y": 42}))
+console.log(isEmpty({}))
+console.log(isEmpty([null, false, 0]))
